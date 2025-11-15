c5a3569e9b4ffdd07259f2b2dff909a0b347e776
Elpacos <vicroc.vn@gmail.com>
2025-11-15 18:55:33 +0100

    chore: implement  hook setters

--- test/enigma-dark-invariants/hooks/DefaultBeforeAfterHooks.t.sol
+++ test/enigma-dark-invariants/hooks/DefaultBeforeAfterHooks.t.sol
@@ -32,7 +32,10 @@ abstract contract DefaultBeforeAfterHooks is BaseHooks {
     function _setUpDefaultHooks() internal {}
 
     /// @notice Helper to initialize storage arrays of default vars
-    function _setUpDefaultVars(DefaultVars storage _defaultVars) internal {}
+    function _setUpDefaultVars(DefaultVars storage _defaultVars) internal {
+        _defaultVars.totalAssets = aaveV3ATokenWrapper.totalAssets();
+        _defaultVars.totalSupply = aaveV3ATokenWrapper.totalSupply();
+    }
 
     ///////////////////////////////////////////////////////////////////////////////////////////////
     //                                           HOOKS                                           //
